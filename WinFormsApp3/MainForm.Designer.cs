189e9763b1678d8886af839eec986d15ab176e56
정환 김 <wjgk0822@naver.com>
2023-11-01 01:39:42 +0900

    Add panel at MainForm.cs

--- WinFormsApp3/MainForm.Designer.cs
+++ WinFormsApp3/MainForm.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.top_panel = new System.Windows.Forms.Panel();
+            this.side_panel = new System.Windows.Forms.Panel();
+            this.bottom_panel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // top_panel
+            // 
+            this.top_panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.top_panel.Location = new System.Drawing.Point(187, 0);
+            this.top_panel.Name = "top_panel";
+            this.top_panel.Size = new System.Drawing.Size(750, 158);
+            this.top_panel.TabIndex = 1;
+            // 
+            // side_panel
+            // 
+            this.side_panel.BackColor = System.Drawing.SystemColors.GrayText;
+            this.side_panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.side_panel.Location = new System.Drawing.Point(0, 0);
+            this.side_panel.Name = "side_panel";
+            this.side_panel.Size = new System.Drawing.Size(187, 652);
+            this.side_panel.TabIndex = 2;
+            // 
+            // bottom_panel
+            // 
+            this.bottom_panel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.bottom_panel.Location = new System.Drawing.Point(187, 502);
+            this.bottom_panel.Name = "bottom_panel";
+            this.bottom_panel.Size = new System.Drawing.Size(750, 150);
+            this.bottom_panel.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(937, 652);
+            this.Controls.Add(this.bottom_panel);
+            this.Controls.Add(this.top_panel);
+            this.Controls.Add(this.side_panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.IsMdiContainer = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MainForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Panel top_panel;
+        private Panel side_panel;
+        private Panel bottom_panel;
     }
 }
